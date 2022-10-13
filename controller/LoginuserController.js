bc404ae07dd9d5419d91eef35875a266ef88bc52
ModiBhargav98 <modibhargav1998@gmail.com>
2022-10-13 22:08:44 +0530

    new changes are added

--- controller/LoginuserController.js
+++ controller/LoginuserController.js
@@ -4,38 +4,52 @@ const UserDb = require("../model/user");
 const jwt = require("jsonwebtoken");
 
 exports.signupUser = catchAsyncError(async (req, res, next) => {
-  console.log(req.params.emailid)
-  console.log(req.query.contacts)
+  console.log(req.params.emailid);
+  console.log(req.query.contacts);
   const userDetail = await UserDb.findOne({ email: req.params.emailid });
-  if(!userDetail){
-    const numbers = req.query.contacts
-    let uniqueNumber = []
-    numbers.forEach(element => {
-      const result = element.substring(0,2);
-      if(result == 91){
-         const num = element.split(91)[1].trim();
-         uniqueNumber.push(num);
-      }else{
-        uniqueNumber.push(element);
+  const contactList = req.query.contacts;
+  let uniqueNumber = [];
+  const findDublicateNumber = (numbers) => {
+    numbers.forEach((element) => {
+      const result = element.substring(0, 2);
+      if (result == 91) {
+        const num = element.split(91)[1].trim();
+        if (num.length === 10 && !isNaN(num)) {
+          uniqueNumber.push(parseInt(num));
+        }
+      } else {
+        if (element.length === 10 && !isNaN(element)) {
+          uniqueNumber.push(parseInt(element));
+        }
       }
     });
-    console.log(uniqueNumber) 
-  }else{
-
+    return [...new Set(uniqueNumber)];
+  };
+  if (!userDetail) {
+    if (contactList?.length > 0) {
+      const number = findDublicateNumber(contactList);
+      if (number?.length > 0) {
+        console.log(typeof number[0]);
+        const createUser = await UserDb.create({
+          email: req.params.emailid,
+          contacts: number,
+        });
+        console.log(createUser);
+      } else {
+        res
+          .status(200)
+          .json({ success: true, msg: "Please add your contact number" });
+      }
+    } else {
+      res
+        .status(200)
+        .json({ success: true, msg: "Please add your contact number" });
+    }
+  } else {
+    if (contactList?.length > 0) {
+      res
+        .status(200)
+        .json({ success: true, msg: "Please add your contact number" });
+    }
   }
-  // if (!emiDetail) {
-  //   const userData = await UserDb.create({
-  //     email: req.body.email,
-  //     password: req.body.password,
-  //   });
-  //   res.status(200).json({
-  //     success: true,
-  //     msg: userData,
-  //   });
-  // } else {
-  //   res.status(200).json({
-  //     success: false,
-  //     msg: "This email already has an account",
-  //   });
-  // }
 });
--- model/user.js
+++ model/user.js
@@ -1,17 +1,14 @@
-const mongoose = require("mongoose")
-const validator = require("validator")
+const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
-    email:{
-        type:String,
-        required:[true,"Please Enter Your Email"],
-        unique:true,
-        validate:[validator.isEmail,"Please Enter a valid email"]
-    },
-    contacts:[{
-        type:String,
-        require:true
-    }]
-})
+  email: {
+    type: String,
+    required: [true, "Please Enter Your Email"],
+    unique: true,
+    validate: [validator.isEmail, "Please Enter a valid email"],
+  },
+  contacts: [{ type: Number, require: true }],
+});
 
-module.exports = mongoose.model("user",userSchema)
+module.exports = mongoose.model("user", userSchema);
